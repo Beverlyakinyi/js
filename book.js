c3588498f30f3c23a0d8dc519b593fb086eec5a3
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-11 09:10:53 +0300

    added files

---
+++ book.js
@@ -0,0 +1,32 @@
+// // Write a JavaScript expression using the modulus operator to find the remainder when 15 is divided by 4.
+
+var a=15
+var b=4
+var d=15/4
+console.log(d)
+// What is the difference between the "==" and "===" operators in JavaScript? Provide an example.
+let val1=10
+let val2=10
+console.log(val1==val2)
+
+let val3=14
+let val4=16
+console.log(val3===val4)
+
+// Write a JavaScript expression to concatenate two strings.
+var name="Beverly"
+var word="Akinyi"
+var wording="Beverly" + " " +  "Akinyi"
+console.log(wording)
+// Declare a variable in JavaScript and assign it the value of a number. Determine its data type using the typeof operator.
+var x=10
+console.log(typeof(x))
+// Explain the difference between null and undefined in JavaScript.
+let y=null
+console.log(null)
+let undefined="money"
+console.log(undefined)
+// Create an array in JavaScript and demonstrate how to access the third element.
+const items=['book', 'ink' ,'book' ];
+console.log(items.at(2));
+
